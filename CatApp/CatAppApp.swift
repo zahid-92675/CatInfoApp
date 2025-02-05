3b2ce23ffdf2cb199966d7a640e52a4a69770a53
zahid <zeequartic@gmail.com>
2025-02-05 12:28:09 -0800

    Initial Commit

---
+++ CatApp/CatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAppApp.swift
+//  CatApp
+//
+//  Created by Zahid Gul on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CatApp
+//
+//  Created by Zahid Gul on 2/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
